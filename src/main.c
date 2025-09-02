89bd24bc17e7d60bf2409193ac5f966bb45ee929
Eke-Martin Paas <eke-martin.paas@agileworks.eu>
2025-09-02 10:39:52 +0300

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,80 +1,89 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @file Sample app to demonstrate PWM.
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/gpio.h>
 
-static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+/* Button 1 (SW1) */
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
+
+/* LED0 for feedback */
+static const struct gpio_dt_spec rock_pin = GPIO_DT_SPEC_GET(DT_ALIAS(rock), gpios);
+
+
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+
+
 
 #define MIN_PERIOD PWM_SEC(1U) / 128U
 #define MAX_PERIOD PWM_SEC(1U)
 
+static struct gpio_callback button_cb;
+
+int turned_on = 0;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    printk(" Button was pressed!\n");
+
+    // Turn on LED0
+    gpio_pin_set_dt(&led1, turned_on);
+    
+    // Turn on ROCK ;)
+    gpio_pin_set_dt(&rock_pin, turned_on);
+    
+    turned_on = !turned_on;
+}
+
 int main(void)
 {
-	uint32_t max_period;
-	uint32_t period;
-	uint8_t dir = 0U;
-	int ret;
+    int ret;
 
-	printk("PWM-based blinky\n");
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) {
+        printk(" Failed to configure button pin: %d\n", ret);
+        return 0;
+    }
 
-	if (!pwm_is_ready_dt(&pwm_led0)) {
-		printk("Error: PWM device %s is not ready\n",
-		       pwm_led0.dev->name);
-		return 0;
-	}
+    if (!device_is_ready(led1.port)) {
+        printk(" LED device %s not ready\n", led1.port->name);
+        return 0;
+    }
 
-	/*
-	 * In case the default MAX_PERIOD value cannot be set for
-	 * some PWM hardware, decrease its value until it can.
-	 *
-	 * Keep its value at least MIN_PERIOD * 4 to make sure
-	 * the sample changes frequency at least once.
-	 */
-	printk("Calibrating for channel %d...\n", pwm_led0.channel);
-	max_period = MAX_PERIOD;
-	while (pwm_set_dt(&pwm_led0, max_period, max_period / 2U)) {
-		max_period /= 2U;
-		if (max_period < (4U * MIN_PERIOD)) {
-			printk("Error: PWM device "
-			       "does not support a period at least %lu\n",
-			       4U * MIN_PERIOD);
-			return 0;
-		}
-	}
+    if (!device_is_ready(rock_pin.port)) {
+        printk(" LED device %s not ready\n", led1.port->name);
+        return 0;
+    }
 
-	printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
-	       max_period, MIN_PERIOD);
+    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT);
+    if (ret < 0) {
+        printk(" Failed to configure LED: %d\n", ret);
+        return 0;
+    }
 
-	period = max_period;
-	while (1) {
-		ret = pwm_set_dt(&pwm_led0, period, period / 2U);
-		if (ret) {
-			printk("Error %d: failed to set pulse width\n", ret);
-			return 0;
-		}
-		printk("Using period %d\n", period);
+    ret = gpio_pin_configure_dt(&rock_pin, GPIO_OUTPUT);
+    if (ret < 0) {
+        printk(" Failed to configure LED: %d\n", ret);
+        return 0;
+    }
 
-		period = dir ? (period * 2U) : (period / 2U);
-		if (period > max_period) {
-			period = max_period / 2U;
-			dir = 0U;
-		} else if (period < MIN_PERIOD) {
-			period = MIN_PERIOD * 2U;
-			dir = 1U;
-		}
+    gpio_pin_set_dt(&led1, 0);  // Off
+    gpio_pin_set_dt(&rock_pin, 0);  // Off
 
-		k_sleep(K_SECONDS(4U));
-	}
-	return 0;
+    // === 3. Setup Interrupt ===
+    gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
+    ret = gpio_add_callback(button.port, &button_cb);
+    if (ret < 0) {
+        printk(" Failed to add callback: %d\n", ret);
+        return 0;
+    }
+
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret < 0) {
+        printk(" Failed to configure interrupt: %d\n", ret);
+        return 0;
+    }
+
+
+    return 0;
 }
