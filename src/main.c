e992043f7551da61d48de51a6eb4378f406a0411
Eke-Martin Paas <eke-martin.paas@agileworks.eu>
2025-09-29 17:57:26 +0300

    RAW-AT HTTP/POST-webhk

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
-/* - successful connection v0.1 - HTTP */
-/* note, SHREAD broken a. reply - 29/9 */
+
+/*
+HTTP(s) POST TEST SW RAW AT-COMMANDS SIM7080-NRF52/52832wUART
+    ZEPHYR 4.1.199 / NCS v3.1.0
+
+    :Zephyr SIMCOM-SIM7080 NETWORKING STACK IS TOO FUCKING BIG FOR 52832 (RAM)
+        >RAW AT reduces it by 6X!
+    :next GET&POST (huge)
+*/
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
@@ -7,7 +14,7 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/gpio.h>
 #include <string.h>
-#include <stdlib.h> // for atoi
+#include <stdlib.h>
 
 // UART & Button
 const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(uart0));
@@ -21,7 +28,7 @@ static size_t resp_len = 0;
 static bool response_complete = false;
 static bool waiting_for_response = false;
 static bool last_command_successful = false;
-static int last_http_data_size = 0; // NEW: Store HTTP response data size
+static int last_http_data_size = 0;
 
 // Button work
 static struct k_work button_work;
@@ -32,8 +39,8 @@ void send_at_command(const char *cmd);
 void run_http_test(void);
 void run_communication_test(void);
 
-// UART callback - Updated to parse HTTP response data size
-static void uart_evt_cb(const struct device *dev, struct uart_event *evt, struct uart_event *user_data)
+// UART callback - Fixed version
+static void uart_evt_cb(const struct device *dev, struct uart_event *evt, void *user_data)
 {
     ARG_UNUSED(dev);
     ARG_UNUSED(user_data);
@@ -67,7 +74,7 @@ static void uart_evt_cb(const struct device *dev, struct uart_event *evt, struct
                         waiting_for_response = false;
                         last_command_successful = false;
                     } 
-                    // NEW: Parse HTTP response data size from SHREQ response
+                    // Parse HTTP response data size from SHREQ response
                     else if (strstr(response_buf, "+SHREQ:") != NULL) {
                         // Example: +SHREQ:"POST",200,457
                         char *comma1 = strchr(response_buf, ','); // First comma after method
@@ -235,7 +242,8 @@ void run_http_test(void)
     wait_for_response(K_SECONDS(2));
     k_sleep(K_SECONDS(1));
 
-    send_at_command("AT+SHCONF=\"URL\",\"http://httpbin.org\"");
+    // Use webhook.site URL instead of httpbin.org
+    send_at_command("AT+SHCONF=\"URL\",\"http://webhook.site\"");
     wait_for_response(K_SECONDS(2));
 
     send_at_command("AT+SHCONF=\"BODYLEN\",1024");
@@ -269,6 +277,7 @@ void run_http_test(void)
         send_at_command("AT+SHCHEAD");
         wait_for_response(K_SECONDS(2));
 
+        // Use webhook.site specific headers
         send_at_command("AT+SHAHEAD=\"User-Agent\",\"nRF52-SIM7080\"");
         wait_for_response(K_SECONDS(2));
 
@@ -294,13 +303,14 @@ void run_http_test(void)
             for (int i = 0; post_data[i] != '\0'; i++) {
                 uart_poll_out(uart0, post_data[i]);
             }
-            uart_poll_out(uart0, 0x1A);
+            uart_poll_out(uart0, 0x1A); // CTRL+Z
             printk(">>> Sent HTTP body + CTRL+Z\n");
 
-            send_at_command("AT+SHREQ=\"/post\",3");
+            // Use your specific webhook URL
+            send_at_command("AT+SHREQ=\"/fd5cf81a-76a1-4d86-96ef-a883745fcf88\",3");
             wait_for_response(K_SECONDS(20)); // This will parse the data size
             
-            // NEW: Read the response using the actual data size returned by SHREQ
+            // Read the response using the actual data size returned by SHREQ
             if (last_command_successful && last_http_data_size > 0) {
                 char shread_cmd[30];
                 snprintf(shread_cmd, sizeof(shread_cmd), "AT+SHREAD=0,%d", last_http_data_size);
@@ -372,6 +382,7 @@ int main(void)
     }
 }
 
+
 /*
 
 CONFIG_UART_CONSOLE=n
@@ -390,5 +401,335 @@ CONFIG_UART_ASYNC_API=y
 # Minimal memory usage:
 CONFIG_HEAP_MEM_POOL_SIZE=1024
 CONFIG_MAIN_STACK_SIZE=1024
+
+*/
+
+/*
+def. dts -- most of dts USELESS DUE TO USING RAW AT INSTEAD 
+
+chosen {
+		zephyr,console = &uart0;
+		zephyr,shell-uart = &uart0;
+		zephyr,uart-mcumgr = &uart0;
+		zephyr,bt-mon-uart = &uart0;
+		zephyr,bt-c2h-uart = &uart0;
+		zephyr,sram = &sram0;
+		zephyr,flash = &flash0;
+		zephyr,code-partition = &slot0_partition;
+	};
+
+	leds {
+		compatible = "gpio-leds";
+
+		led0: led_0 {
+			gpios = <&gpio0 17 GPIO_ACTIVE_LOW>;
+			label = "Green LED 0";
+		};
+
+		led1: led_1 {
+			gpios = <&gpio0 18 GPIO_ACTIVE_LOW>;
+			label = "Green LED 1";
+		};
+
+		led2: led_2 {
+			gpios = <&gpio0 19 GPIO_ACTIVE_LOW>;
+			label = "Green LED 2";
+		};
+
+		led3: led_3 {
+			gpios = <&gpio0 20 GPIO_ACTIVE_LOW>;
+			label = "Green LED 3";
+		};
+	};
+
+	pwmleds {
+		compatible = "pwm-leds";
+
+		pwm_led0: pwm_led_0 {
+			pwms = <&pwm0 0 PWM_MSEC(20) PWM_POLARITY_INVERTED>;
+		};
+	};
+
+	buttons {
+		compatible = "gpio-keys";
+		wakeup-source;
+
+		button0: button_0 {
+			gpios = <&gpio0 13 (GPIO_PULL_UP | GPIO_ACTIVE_LOW)>;
+			label = "Push button switch 0";
+			zephyr,code = <INPUT_KEY_0>;
+		};
+
+		button1: button_1 {
+			gpios = <&gpio0 14 (GPIO_PULL_UP | GPIO_ACTIVE_LOW)>;
+			label = "Push button switch 1";
+			zephyr,code = <INPUT_KEY_1>;
+		};
+
+		button2: button_2 {
+			gpios = <&gpio0 15 (GPIO_PULL_UP | GPIO_ACTIVE_LOW)>;
+			label = "Push button switch 2";
+			zephyr,code = <INPUT_KEY_2>;
+		};
+
+		button3: button_3 {
+			gpios = <&gpio0 16 (GPIO_PULL_UP | GPIO_ACTIVE_LOW)>;
+			label = "Push button switch 3";
+			zephyr,code = <INPUT_KEY_3>;
+		};
+	};
+
+	gpio_trigger {
+		compatible = "gpio-leds";
+		trigger_pin: trigger_0 {
+			gpios = <&gpio0 11 GPIO_ACTIVE_HIGH>;
+			label = "Wakeup Trigger Pin"; //sbc wakeup
+		};
+		trigger_pin2: trigger_1 {
+			gpios = <&gpio0 12 GPIO_ACTIVE_HIGH>;
+			label = "Wakeup Trigger Pin 2"; //modem wakeup
+		};
+
+	};
+
+	gpio_wake {
+		compatible = "gpio-keys";
+		wakeup-source;
+		wake_pin: wake_pin {
+			gpios = <&gpio0 28 (GPIO_PULL_UP | GPIO_ACTIVE_LOW)>;
+			label = "Wakeup Pin"; //sbc interrupting sbc for wake
+			zephyr,code = <INPUT_KEY_4>;
+		};
+	};
+
+
+    aliases {
+		led0 = &led0;
+		led1 = &led1;
+		led2 = &led2;
+		led3 = &led3;
+		pwm-led0 = &pwm_led0;
+		sw0 = &button0;
+		sw1 = &button1;
+		sw2 = &button2;
+		sw3 = &button3;
+		bootloader-led0 = &led0;
+		mcuboot-button0 = &button0;
+		mcuboot-led0 = &led0;
+		watchdog0 = &wdt0;
+		trigger0 = &trigger_pin;
+		trigger1 = &trigger_pin2;
+		wakepin = &wake_pin;
+		modem = &sim7080;
+	};
+
+    arduino_serial: &uart0 {
+	status = "okay";
+	compatible = "nordic,nrf-uarte";
+	current-speed = <115200>;
+	pinctrl-0 = <&uart0_default>;
+	pinctrl-1 = <&uart0_sleep>;
+	pinctrl-names = "default", "sleep";
+	
+	#address-cells = <1>;
+	#size-cells = <0>;
+	
+	sim7080: modem@0 {
+			compatible = "simcom,sim7080";
+			reg = <0>;
+			mdm-power-gpios = <&gpio0 2 GPIO_ACTIVE_LOW>;
+            status = "okay";
+            };
+    };
+
+    &rng {
+	status = "okay";
+    };
+*/
+
+
+
+/*
+
+SAMPLE OUTPUT (webhook) RTT, IDK why it it fails to show on website itself wtf
+
+00> *** Booting nRF Connect SDK v3.1.0-6c6e5b32496e ***
+00> *** Using Zephyr OS v4.1.99-1612683d4010 ***
+00> Starting application...
+00> UART device found: uart@40002000
+00> Press button (SW0) to run Communication Test
+00> Ready. Monitoring UART...
+00> 
+00> --- BUTTON PRESSED: Running Communication Test ---
+00> --- Starting Communication Test ---
+00> >>> AT
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> ATI
+00> Waiting for response for up to 0 seconds...
+00> <<< R1951.07
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGMR
+00> Waiting for response for up to 0 seconds...
+00> <<< Revision:1951B16SIM7080
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGMI
+00> Waiting for response for up to 0 seconds...
+00> <<< SIMCOM_Ltd
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> ATE1
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> ATE0
+00> Waiting for response for up to 0 seconds...
+00> <<< ATE0
+00> <<< OK
+00> <<< Response received within timeout.
+00> --- Communication Test Complete ---
+00> --- Now Running HTTP Test ---
+00> --- Starting HTTP Test ---
+00> >>> AT
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> ATE0
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CPIN?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CPIN: READY
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGREG=1
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGREG?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CGREG: 1,1
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGATT?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CGATT: 1
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CSQ
+00> Waiting for response for up to 0 seconds...
+00> <<< +CSQ: 30,99
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CPSI?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CPSI: LTE NB-IOT,Online,248-01,0x2AFA,51716400,177,EUTRAN-BAND20,6254,0,0,-10,-63,-54,15
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGDCONT=1,"IP","internet.telia.ee"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CNACT=0,1
+00> Waiting for response for up to 0 seconds...
+00> <<< ERROR
+00> <<< Response received within timeout.
+00> >>> AT+CNACT?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CNACT: 0,1,"10.32.116.31"
+00> <<< +CNACT: 1,0,"0.0.0.0"
+00> <<< +CNACT: 2,0,"0.0.0.0"
+00> <<< +CNACT: 3,0,"0.0.0.0"
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CGREG?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CGREG: 1,1
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHCONF="URL","http://webhook.site"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHCONF="BODYLEN",1024
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHCONF="HEADERLEN",350
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> Waiting for network to stabilize...
+00> ..............................
+00> >>> AT+CGATT?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CGATT: 1
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+CNACT?
+00> Waiting for response for up to 0 seconds...
+00> <<< +CNACT: 0,1,"10.32.116.31"
+00> <<< +CNACT: 1,0,"0.0.0.0"
+00> <<< +CNACT: 2,0,"0.0.0.0"
+00> <<< +CNACT: 3,0,"0.0.0.0"
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHCONN
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHSTATE?
+00> Waiting for response for up to 0 seconds...
+00> <<< +SHSTATE: 1
+00> <<< OK
+00> <<< Response received within timeout.
+00> AT+SHCONN successful, proceeding with HTTP request.
+00> >>> AT+SHCHEAD
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHAHEAD="User-Agent","nRF52-SIM7080"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHAHEAD="Cache-control","no-cache"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHAHEAD="Connection","keep-alive"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHAHEAD="Accept",""
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHAHEAD="Content-Type","application/json"
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHBOD=39,5000
+00> Waiting for response for up to 0 seconds...
+00> <<< > 
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> Sent HTTP body + CTRL+Z
+00> >>> AT+SHREQ="/fd5cf81a-76a1-4d86-96ef-a883745fcf88",3
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> >>> AT+SHREAD=0,500
+00> Waiting for response for up to 0 seconds...
+00> <<< ERROR
+00> <<< Response received within timeout.
+00> >>> AT+SHDISC
+00> Waiting for response for up to 0 seconds...
+00> <<< OK
+00> <<< Response received within timeout.
+00> --- HTTP test complete ---
 
 */
